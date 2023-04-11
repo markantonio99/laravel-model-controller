e736d1201c0c37a3eff6bd26b644adb424533fbf
“markantonio99” <markomakkia@gmail.com>
2023-04-11 19:40:03 +0200

    fine

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function homepage()
+    {
+        $movies = Movie::all();
+
+        $data = [
+            'movies' => $movies
+        ];
+       
+        return view('welcome', $data); 
+    }
+}
